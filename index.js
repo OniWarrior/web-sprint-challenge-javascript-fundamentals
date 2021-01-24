5184d5ac295094b94c4d1dd02a14fb78bfb8a03e
Stephen Aranda <aranda.stephen88@gmail.com>
2021-01-23 20:30:43 -0600

    Completed request 4 by creating two nested functions. First function created an array that had all of the animal populations. The second was the callback function which was then sent to .reduce to calculate sum of population

--- index.js
+++ index.js
@@ -106,9 +106,25 @@ animalNames(zooAnimals);
   Remember the reduce method takes two arguments: a callback (which itself takes two args - the accumulator and the item), and an initial value for the count.
   */
 
-  function USApop(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
+ function USApop(animals){
+
+  function animalPopulation()
+   {
+     return animals.map(match => match.population);
+   }
+
+   const populationTotal = function(accumulator,num)
+   {
+     return accumulator + num;
+
+   }
+   
+   const population = animalPopulation();
+   return population.reduce(populationTotal,0);
+ }
+
+ USApop(zooAnimals);
+
   
   
   // 🦁🦁🦁 Callbacks 🦁🦁🦁  
